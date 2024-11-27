90eb0758d35621e6813723a403caca115a56778c
mdenjamulislam <mdenjamulislam527@gmail.com>
2024-11-27 20:36:27 +0600

    Init router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from '@material-tailwind/react'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  }
+])
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <ThemeProvider>
-            <App />
+            <RouterProvider router={router}/>
         </ThemeProvider>
     </StrictMode>,
 );
